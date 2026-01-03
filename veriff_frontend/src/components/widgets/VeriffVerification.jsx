a637c4b7c2fb67c524cbae0e5e6b28ec723908da
Sagnik Das <sagnik@idenza.ai>
2026-01-03 23:53:30 +0530

    updated veriff component

--- veriff_frontend/src/components/widgets/VeriffVerification.jsx
+++ veriff_frontend/src/components/widgets/VeriffVerification.jsx
@@ -1,7 +1,13 @@
 import { useEffect, useRef } from "react";
 import { Veriff } from "@veriff/js-sdk";
+import Logo from "../../assets/logo.svg";
 
-export default function VeriffVerification({ subscriberId, email, firstName, lastName }) {
+export default function VeriffVerification({
+  subscriberId,
+  email,
+  firstName,
+  lastName,
+}) {
   const initialized = useRef(false);
 
   useEffect(() => {
@@ -12,11 +18,10 @@ export default function VeriffVerification({ subscriberId, email, firstName, las
     const vendorData = JSON.stringify({
       subscriberId,
       email,
-      
     });
 
     const veriff = Veriff({
-      apiKey: 'f680f797-4076-4e73-9ee4-d54d3a635ac1',
+      apiKey: "f680f797-4076-4e73-9ee4-d54d3a635ac1",
       parentId: "veriff-root",
 
       onSession: (err, response) => {
@@ -44,5 +49,45 @@ export default function VeriffVerification({ subscriberId, email, firstName, las
     });
   }, [subscriberId, email, firstName, lastName]);
 
-  return <div id="veriff-root" />;
+   return (
+    <section className="min-h-screen bg-gr flex items-center justify-center">
+      <div className="h-full max-w-6xl px-6 bg-slate-100" >
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-14 items-center ">
+
+          {/* Left content */}
+          <div className="bg-white p-8 mt-10 mb-10 rounded-2xl">
+            {/* Logo */}
+            <img
+              src={Logo}
+              alt="Company logo"
+              className="h-20 mb-10"
+            />
+
+            <h1 className="text-4xl font-semibold text-gray-900 leading-tight">
+              Identity verification
+            </h1>
+
+            <p className="mt-4 text-lg text-gray-600 max-w-md">
+              Secure identity checks to protect your account and meet compliance
+              requirements.
+            </p>
+
+            <ul className="mt-6 space-y-2 text-sm text-gray-500">
+              <li>• Takes less than 2 minutes</li>
+              <li>• Keep your govt ID with you</li>
+              <li>• Please check if your camera is working</li>
+            </ul>
+          </div>
+
+          {/* Right card */}
+          <div className="flex justify-center">
+            <div className="w-full max-w-md rounded-2xl bg-white p-8">
+              <div id="veriff-root" style={{scale: "1.1"}}/>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+  );
 }
