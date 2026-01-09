adeef8cb1da81012413318cc462ae0982d0e5a8f
Sagnik Das <sagnik@idenza.ai>
2026-01-09 11:17:45 +0530

    removed not required get param

--- idv_frontend/src/components/VerificationLoading.jsx
+++ idv_frontend/src/components/VerificationLoading.jsx
@@ -36,7 +36,7 @@ export default function VerificationLoading() {
         }
 
         const res = await fetch(
-          `${SERVER_URL}/idv/verification/status?trackingId=${trackingId}&ngrok-skip-browser-warning=1`
+          `${SERVER_URL}/idv/verification/status?trackingId=${trackingId}`
         );
 
         console.log(
